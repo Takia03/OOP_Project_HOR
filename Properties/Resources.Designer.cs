35407d394c894a3150173b796fd43a9737d8cfa3
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2023-04-21 00:39:14 +0600

    CreatedSearch

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace OOP_Project_HOR.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap a {
+            get {
+                object obj = ResourceManager.GetObject("a", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap hotr2crp {
             get {
                 object obj = ResourceManager.GetObject("hotr2crp", resourceCulture);
---
+++ Search_UI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Project_HOR
+{
+    public partial class Search_UI : Form
+    {
+        public Search_UI()
+        {
+            InitializeComponent();
+        }
+
+        private void home_button_Click(object sender, EventArgs e)
+        {
+            HomePage h5 = new HomePage();
+            h5.Show();
+            this.Hide();
+        }
+    }
+}
